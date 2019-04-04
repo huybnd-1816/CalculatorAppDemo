427d67334d2541037f152c8a8639b2c6867c325c
Nguyen Duc Huy <nguyen.duc.huyb@sun-asterisk.com>
2019-04-04 09:25:46 +0700

    Fix comment 2

--- CalculatorApp/CalculatorApp/ViewController.swift
+++ CalculatorApp/CalculatorApp/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
     
     var tempValue: Double?
     var currentOperator: Operator?
-    var check = true //If user press button Equal
+    var isEqualButtonTapped = true //If user tap button Equal
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,60 +34,62 @@ class ViewController: UIViewController {
 
     @IBAction func handleACtapped(_ sender: Any) {
         lblResult.text = "0"
-        check = true
+        isEqualButtonTapped = true
         tempValue = 0.0
         currentOperator = nil
         resetState()
     }
     
     @IBAction func handleNumbersTapped(_ sender: UIButton) {
-        if (sender.tag == 0 && lblResult.text?.first == "0"){
+        if sender == btnNumberActions[0] && lblResult.text?.first == "0" {
             return
         }
         
         guard (lblResult.text?.count)! < 11 else { return }
+        guard let buttonValue = sender.titleLabel?.text else { return }
         
-        if (lblResult.text == "0"){
-            lblResult.text = lblResult.text?.replacingOccurrences(of: "0", with: String(sender.tag))
+        if lblResult.text == "0" {
+            lblResult.text = lblResult.text?.replacingOccurrences(of: "0", with: buttonValue)
         }
-        else if check{
-            lblResult.text?.append(String(sender.tag))
+        else if isEqualButtonTapped {
+            lblResult.text?.append(buttonValue)
         }
         else{
             lblResult.text = ""
-            lblResult.text?.append(String(sender.tag))
-            check = true
+            lblResult.text?.append(buttonValue)
+            isEqualButtonTapped = true
         }
     }
     
     
     @IBAction func handleOperatorTapped(_ sender: UIButton) {
         tempValue = Double(lblResult.text!)
-        check = false
+        isEqualButtonTapped = false
         
         resetState()
         
         switch sender {
-        case btnOperatorsAction[0]:
-            currentOperator = Operator.plus
-        case btnOperatorsAction[1]:
-            currentOperator = Operator.subtract
-        case btnOperatorsAction[2]:
-            currentOperator = Operator.mutiply
-        case btnOperatorsAction[3]:
-            currentOperator = Operator.divide
-        default:
-            print("Error")
+            case btnOperatorsAction[0]:
+                currentOperator = Operator.plus
+            case btnOperatorsAction[1]:
+                currentOperator = Operator.subtract
+            case btnOperatorsAction[2]:
+                currentOperator = Operator.mutiply
+            case btnOperatorsAction[3]:
+                currentOperator = Operator.divide
+            default:
+                print("Error")
         }
         sender.backgroundColor = .black
     }
     
     @IBAction func handleEqualTapped(_ sender: Any) {
-        check = false
         guard let tempValue = tempValue else { return }
         guard currentOperator != nil else { return }
+
         let currentResult = Double(lblResult.text!)!
-        let finalResult:Double?
+        let finalResult: Double?
+        isEqualButtonTapped = false
         
         switch currentOperator! {
             case .plus:
@@ -106,24 +108,23 @@ class ViewController: UIViewController {
         }
         
         lblResult.text = removeTrailingZero(temp: finalResult!)
-        
         resetState()
     }
     
     @IBAction func handleDotTapped(_ sender: Any) {
         if !(lblResult.text?.contains("."))!{
-            if (!check){
+            if !isEqualButtonTapped {
                 lblResult.text = "0"
             }
             lblResult.text?.append(".")
-            check = true
+            isEqualButtonTapped = true
         }
     }
     
     @IBAction func handlePosNegTapped(_ sender: Any) {
-        var number:Double = Double(lblResult.text!)!
+        var number = Double(lblResult.text!)!
         
-        if (number > 0.0 || number < 0.0){
+        if number > 0.0 || number < 0.0 {
             number = -number;
         }
         
@@ -131,9 +132,9 @@ class ViewController: UIViewController {
     }
     
     @IBAction func handlePercentageTapped(_ sender: Any) {
-        var number:Double = Double(lblResult.text!)!
+        var number = Double(lblResult.text!)!
         
-        if (number != 0.0){
+        if number != 0.0 {
             number = number / 100
             lblResult.text = removeTrailingZero(temp: number)
         }
@@ -150,9 +151,8 @@ extension ViewController {
     //TODO: Return Original State
     func resetState()
     {
-        btnOperatorsAction[0].backgroundColor = UIColor(red: 247/255, green: 146/255, blue: 49/255, alpha: 1.0)
-        btnOperatorsAction[1].backgroundColor = UIColor(red: 247/255, green: 146/255, blue: 49/255, alpha: 1.0)
-        btnOperatorsAction[2].backgroundColor = UIColor(red: 247/255, green: 146/255, blue: 49/255, alpha: 1.0)
-        btnOperatorsAction[3].backgroundColor = UIColor(red: 247/255, green: 146/255, blue: 49/255, alpha: 1.0)
+        for index in 0...3 {
+            btnOperatorsAction[index].backgroundColor = UIColor(red: 247/255, green: 146/255, blue: 49/255, alpha: 1.0)
+        }
     }
 }
